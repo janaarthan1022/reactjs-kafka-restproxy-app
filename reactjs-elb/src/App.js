6d75f807145411dfba33d16fd681a366fb863b64
janaarthan1022 <janaarthanarch@gmail.com>
2025-07-31 00:17:59 +0530

    fetch added

--- reactjs-elb/src/App.js
+++ reactjs-elb/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [host, setHost] = useState("")
+  async function elb() {
+    const res = await fetch("/");
+    const text = await res.text();
+    setHost(text);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>AWS Elastic Load Balancer Testing</h1>
+      <button onClick={elb}>Elastic Load Balancer</button>
+      <h2>{host}</h2>
     </div>
   );
 }
